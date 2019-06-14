ba3b34bf8e845ff1aa7a94a7a067dc22c489f877
Matt Cameron <cameron.matt@gmail.com>
2019-06-14 10:41:02 -0500

    add AuthorsController and initial migration

---
+++ CS321_W3D2_BookAPI/Controllers/AuthorsController.cs
@@ -0,0 +1,80 @@
+﻿using CS321_W3D2_BookAPI.Models;
+using CS321_W3D2_BookAPI.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CS321_W3D2_BookAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthorsController : ControllerBase
+    {
+        private readonly IAuthorService _authorService;
+
+        // Constructor
+        public AuthorsController(IAuthorService authorService)
+        {
+            // TODO: keep a reference to the service so we can use below
+            _authorService = authorService;
+        }
+
+        // TODO: get all authors
+        // GET api/authors
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_authorService.GetAll());
+        }
+
+        // get specific author by id
+        // GET api/authors/:id
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var author = _authorService.Get(id);
+            if (author == null) return NotFound();
+            return Ok(author);
+        }
+
+        // create a new author
+        // POST api/authors
+        [HttpPost]
+        public IActionResult Post([FromBody] Author newAuthor)
+        {
+            try
+            {
+                // add the new author
+                _authorService.Add(newAuthor);
+            }
+            catch (System.Exception ex)
+            {
+                ModelState.AddModelError("AddAuthor", ex.Message);
+                return BadRequest(ModelState);
+            }
+
+            // return a 201 Created status. This will also add a "location" header
+            // with the URI of the new author. E.g., /api/authors/99, if the new is 99
+            return CreatedAtAction("Get", new { Id = newAuthor.Id }, newAuthor);
+        }
+
+        // TODO: update an existing author
+        // PUT api/authors/:id
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] Author updatedAuthor)
+        {
+            var author = _authorService.Update(updatedAuthor);
+            if (author == null) return NotFound();
+            return Ok(author);
+        }
+
+        // TODO: delete an existing author
+        // DELETE api/authors/:id
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var author = _authorService.Get(id);
+            if (author == null) return NotFound();
+            _authorService.Remove(author);
+            return NoContent();
+        }
+    }
+}
---
+++ CS321_W3D2_BookAPI/Migrations/20190614153158_InitialCreate.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CS321_W3D2_BookAPI.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Authors",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    FirstName = table.Column<string>(nullable: true),
+                    LastName = table.Column<string>(nullable: true),
+                    BirthDate = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Authors", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Title = table.Column<string>(nullable: true),
+                    AuthorId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Books_Authors_AuthorId",
+                        column: x => x.AuthorId,
+                        principalTable: "Authors",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Authors",
+                columns: new[] { "Id", "BirthDate", "FirstName", "LastName" },
+                values: new object[] { 1, new DateTime(1902, 2, 27, 0, 0, 0, 0, DateTimeKind.Unspecified), "John", "Steinbeck" });
+
+            migrationBuilder.InsertData(
+                table: "Authors",
+                columns: new[] { "Id", "BirthDate", "FirstName", "LastName" },
+                values: new object[] { 2, new DateTime(1947, 9, 21, 0, 0, 0, 0, DateTimeKind.Unspecified), "Stephen", "King" });
+
+            migrationBuilder.InsertData(
+                table: "Books",
+                columns: new[] { "Id", "AuthorId", "Title" },
+                values: new object[] { 1, 1, "The Grapes of Wrath" });
+
+            migrationBuilder.InsertData(
+                table: "Books",
+                columns: new[] { "Id", "AuthorId", "Title" },
+                values: new object[] { 2, 1, "Cannery Row" });
+
+            migrationBuilder.InsertData(
+                table: "Books",
+                columns: new[] { "Id", "AuthorId", "Title" },
+                values: new object[] { 3, 2, "The Shining" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Books_AuthorId",
+                table: "Books",
+                column: "AuthorId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Books");
+
+            migrationBuilder.DropTable(
+                name: "Authors");
+        }
+    }
+}
