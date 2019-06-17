6871367bfaf94d0678b1c4f4eeb3374eb0a1548f
Matt Cameron <cameron.matt@gmail.com>
2019-06-17 14:26:44 -0500

    Add Core and Data projects

---
+++ CS321_W4D1_BookAPI.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace CS321_W4D1_BookAPI.Core
+{
+    public class Class1
+    {
+    }
+}
---
+++ CS321_W4D1_BookAPI.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace CS321_W4D1_BookAPI.Data
+{
+    public class Class1
+    {
+    }
+}
