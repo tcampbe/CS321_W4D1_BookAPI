c086b72b7fe649e7579debdfd5ddd447d5c6316e
Matt Cameron <cameron.matt@gmail.com>
2019-06-18 18:05:15 -0500

    Include author and publisher

--- CS321_W4D1_BookAPI/ApiModels/PublisherModel.cs
+++ CS321_W4D1_BookAPI/ApiModels/PublisherModel.cs
@@ -6,7 +6,11 @@ namespace CS321_W4D1_BookAPI.ApiModels
     public class PublisherModel
     {
         public int Id { get; set; }
-        // TODO: Fill in PublisherModel properties.
+        public string Name { get; set; }
+        public int FoundedYear { get; set; }
+        public string CountryOfOrigin { get; set; }
+        public string HeadQuartersLocation { get; set; }
+        public ICollection<BookModel> Books { get; set; }
     }
 }
 
